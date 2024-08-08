eaa21c378762e14976b6bbf076cb7cf16f1e01f5
Jefferson Duarte <jeffersonsduarte@live.com>
2024-08-08 09:55:16 +0100

    Configurado cadastro de usuarios

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.contrib.messages import constants
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +59,9 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,9 +122,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'templates/static'
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-info',
+    constants.ERROR: 'alert-danger',
+    constants.INFO: 'alert-info',
+    constants.SUCCESS: 'alert-success',
+    constants.WARNING: 'alert-warning',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(
+                request,
+                constants.ERROR,
+                'As senha nao coincidem.'
+            )
+            return redirect('usuarios:cadastro')
+
+        if len(senha) < 6:
+            messages.add_message(
+                request,
+                constants.ERROR,
+                'A senha precisa ter pelomenos 6 digitos.'
+            )
+            return redirect('usuarios:cadastro')
+
+        users = User.objects.filter(username=username)
+
+        if users.exists():
+            messages.add_message(
+                request,
+                constants.ERROR,
+                'Usuario ja existe.'
+            )
+            return redirect('usuarios:cadastro')
+
+        user = User.objects.create_user(
+            username=username,
+            password=senha
+        )
+
+    return redirect('cadastro:logar')
